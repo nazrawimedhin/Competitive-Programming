0d50404876a0dfea3a6fa261d341abe6232e8389
nazrawimedhin <nazrawimedhin1@gmail.com>
2023-03-25 13:28:52 +0300

    Leet Code Challenge: smaller than z current num

---
+++ a2sv-data_structures_and_algorithms/sorting/how-many-nums-r-smaller-than-z-current-num.py
@@ -0,0 +1,10 @@
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        result = []
+        for i in nums:
+            count = 0
+            for j in nums:
+                if j < i:
+                    count = count + 1
+            result.append(count)
+        return result
