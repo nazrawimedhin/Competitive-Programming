536bbc334c9594c72a6ba3823584509a56baf358
Nazrawi Medhin <nazrawimedhin1@gmail.com>
2023-05-26 10:48:05 +0300

    Code Force Challenge: domino piling

--- a2sv-data_structures_and_algorithms/math/domino-piling.py
+++ a2sv-data_structures_and_algorithms/math/domino-piling.py
@@ -2,8 +2,3 @@
 
 def dominoPilling(m, n):
     return int(m * n / 2)
-
-if __name__ =="__main__":
-    m = int(input("Enter M:\n"))
-    n = int(input("Enter N:\n"))
-    print(dominoPilling(m, n))
